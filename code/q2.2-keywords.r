4616fae1d4130ab76e270f37b854d791294b849f
jmarnat <josselin.marnat@gmail.com>
2018-01-09 15:29:47 +0100

    q2.2

--- code/q2.2-keywords.r
+++ code/q2.2-keywords.r
@@ -28,14 +28,65 @@ bad <- cbind(row.names(bad),bad)
 colnames(bad) <- c('doc_id','text')
 
 corpus_bad <- Corpus(DataframeSource(bad))
-
 tm <- tm_map(corpus_bad, removeWords, stopwords("french"))
 whole_txt = apply(data.frame(tm$content),1,paste,collapse=". ")
-controls <- list(removePunctuation=TRUE, tokenize = 'Boost',removeNumbers=TRUE) 
-tf <- (termFreq(whole_txt, control = controls))
+
+ngrams_tokenizer <- function(x)
+  unlist(tokenize_ngrams(x,lowercase = TRUE, n=3, n_min=1))
+controls <- list(removePunctuation=TRUE, tokenize = ngrams_tokenizer,removeNumbers=TRUE, stemming = TRUE) 
+
+tf <- termFreq(whole_txt, control = controls)
 most_freq_tf <- data.frame(findMostFreqTerms(tf, n = 100))
 View(most_freq_tf)
 
 
 
-a <- tokenize_ngrams(whole_txt,lowercase = TRUE, n_min=3L)
+
+corpus <- data.frame()
+setwd('~/Documents/DMBD/DMBD/big_data_project_confidential/')
+for (file in list.files(pattern="SAT*")) {
+  if('Q1bis' %in% colnames(doc1)) next;
+  print(file)
+  doc <- readLines(file)[-2]
+  doc1 <- read.csv(textConnection(doc), header = TRUE, sep = ";")
+
+  if('Q1bis' %in% colnames(doc1)){
+    bad <- data.frame(doc1$Q1bis)
+  } else {
+    mean_satis <- mean(na.omit(doc1[,'Q1']))
+    std_satis <- sd(na.omit(doc1[,'Q1']))
+    
+    note_bad <- mean_satis-std_satis
+    bad <- data.frame(doc1[doc1$Q1 < note_bad,]$Q2)
+    colnames(bad) <- c('comment')
+    bad <- data.frame(bad[bad$comment != '',])
+    colnames(bad) <- c('comment')
+    bad <- data.frame(bad[!is.na(bad$comment),])
+    colnames(bad) <- c('comment')
+    bad <- cbind(row.names(bad),bad)
+    colnames(bad) <- c('doc_id','text')
+  }
+ 
+  corpus <- rbind(corpus,bad)
+}  
+
+corpus_bad <- Corpus(DataframeSource(corpus))
+tm <- tm_map(corpus_bad, removeWords, stopwords("fr"))
+whole_txt = apply(data.frame(tm$content),1,paste,collapse=". ")
+
+ngrams_tokenizer <- function(x)
+  unlist(tokenize_ngrams(x,lowercase = TRUE, n=3, n_min=3))
+ctrls <- list(removePunctuation=TRUE, tokenize = ngrams_tokenizer,removeNumbers=TRUE, stemming=TRUE, wordLengths=c(3,Inf))
+
+tf <- termFreq(whole_txt, control = ctrls)
+most_freq_tf <- data.frame(findMostFreqTerms(tf, n = 100))
+View(most_freq_tf)
+
+
+
+
+
+
+
+
+
