0c32a243d9036743f646f6d6be552bdd5b64598f
remi <remi@lenovo.home>
2018-01-20 18:57:54 +0100

    question n 3-1 fin

--- code/q3_1_remi.R
+++ code/q3_1_remi.R
@@ -96,9 +96,7 @@ data_satisfaction <- data_satisfaction[c("Date.de.réponse","Q1","Meta_donnee.3"
 # Suppression of the na value
 data_satisfaction <- na.omit(data_satisfaction)
 
-# Merge of the two dataframes
-data_satisfaction <- data_satisfaction %>% left_join(data_client, by = c("Meta_donnee.3" = "ID_GRC"))
-
+# Oredring of the dataframe
 data_satisfaction <- data_satisfaction[order(data_satisfaction$Meta_donnee.3, data_satisfaction$Date.de.réponse),]
 
 # http://forums.cirad.fr/logiciel-R/viewtopic.php?t=663
@@ -110,25 +108,20 @@ duplicated2 <- function(x){
   else duplicated(c(x[dup],x))[-(1:sum(dup))]
 }
 
+# Recuparation of the duplicated idgrc
 data_satisfaction <- data_satisfaction[duplicated2(data_satisfaction$Meta_donnee.3),]
 
 # Generation of the dataframe evolution to see the difference between two survey for the same id
 evolution = data.frame()
 for (i in 1:(length(data_satisfaction$Meta_donnee.3)-1)){
   if (data_satisfaction$Meta_donnee.3[i] == data_satisfaction$Meta_donnee.3[i+1]){
-    evolution <- rbind(evolution,c(data_satisfaction$Q1[i+1]-data_satisfaction$Q1[i],data_satisfaction$Q1[i]))
+    evolution <- rbind(evolution,c(data_satisfaction$Meta_donnee.3[i], data_satisfaction$Q1[i+1]-data_satisfaction$Q1[i],data_satisfaction$Q1[i]))
   }
 }
-colnames(evolution) <- c("global","first")
+colnames(evolution) <- c("Meta_donnee.3","global","first")
 
-evolution = data.frame()
-for (i in 1:(length(data_satisfaction$Meta_donnee.3)-1)){
-  if (data_satisfaction$Meta_donnee.3[i] == data_satisfaction$Meta_donnee.3[i+1]){
-    evolution <- rbind(evolution,c(data_satisfaction$Q1[i+1]-data_satisfaction$Q1[i],data_satisfaction$Q1[i],data_satisfaction$TRANCHE_AGE[i],data_satisfaction$NATURE_PERSONNE[i],data_satisfaction$SEGMENTATION_DISTRIBUTIVE[i],data_satisfaction$MARCHE_CSP[i],data_satisfaction$MARCHE_PSO[i],data_satisfaction$TYPOLOGIE[i]))
-  }
-}
-colnames(evolution) <- c("global","first","Tranche d'age","Nature","Segmentation","Marche_CSP","Marche_PSO","Typologie")
-
+# Merge of the two dataframes
+evolution <- evolution %>% left_join(data_client, by = c("Meta_donnee.3" = "ID_GRC"))
 
 # Histogram of evolution
 barplot(t(table(evolution$global)), 
@@ -136,17 +129,101 @@ barplot(t(table(evolution$global)),
         space = 2, las = 1,
         main = "Evolution of the grade between two satisfaction surveys - Global")
 box()
-#dev.print(device = png, file = "Evolution_of_the_grade_between_two_satisfaction_surveys_-_Global.png", width = 600)
+dev.print(device = png, file = "Evolution_of_the_grade_between_two_satisfaction_surveys_-_Global.png", width = 600)
 
-# in function of the first grade
+# according to the first grade
 for (i in 0:10) {
-  title <- paste("Evolution of the grade after a ",toString(i),sep="")
+  title <- paste("Evolution of the grade after a ",toString(i),sep=" ")
   barplot(t(table(evolution$global[evolution$first == i])), 
           horiz = FALSE, ylab = "nb of clients", ylim = c(0,70),
           space = 0.5, las = 1, width = 0.2,
           main = title)
   box()
   file_name <- paste("Evolution_of_the_grade_after_a_",toString(i),".png",sep="")
-  # dev.print(device = png, file = file_name, width = 600)
+  dev.print(device = png, file = file_name, width = 600)
+}
+
+# according to the typologie
+for (i in levels(evolution$TYPOLOGIE)) {
+  if (length(evolution$global[evolution$TYPOLOGIE == i] != 0)){
+  title <- paste("Evolution of the grade according to Typologie",i,sep=" ")
+  barplot(t(table(evolution$global[evolution$TYPOLOGIE == i])), 
+          horiz = FALSE, ylab = "nb of clients", ylim = c(0,70),
+          space = 0.5, las = 1, width = 0.2,
+          main = title)
+  box()
+  file_name <- paste("Evolution_of_the_grade_for_typologie_",i,".png",sep="")
+  dev.print(device = png, file = file_name, width = 600)
+  }
+}
+
+# according to the segmentation
+for (i in levels(evolution$SEGMENTATION_DISTRIBUTIVE)) {
+  if (length(evolution$global[evolution$SEGMENTATION_DISTRIBUTIVE == i] != 0)){
+  title <- paste("Evolution of the grade according to Segmentation",i,sep=" ")
+  barplot(t(table(evolution$global[evolution$SEGMENTATION_DISTRIBUTIVE == i])), 
+          horiz = FALSE, ylab = "nb of clients", ylim = c(0,70),
+          space = 0.5, las = 1, width = 0.2,
+          main = title)
+  box()
+  file_name <- paste("Evolution_of_the_grade_for_segmentation_",i,".png",sep="")
+  dev.print(device = png, file = file_name, width = 600)
+  }
+}
+
+# according to the nature
+for (i in levels(evolution$NATURE_PERSONNE)) {
+  if (length(evolution$global[evolution$NATURE_PERSONNE == i] != 0)){
+  title <- paste("Evolution of the grade according to Nature",i,sep=" ")
+  barplot(t(table(evolution$global[evolution$NATURE_PERSONNE == i])), 
+          horiz = FALSE, ylab = "nb of clients", 
+          space = 0.5, las = 1, width = 0.2,
+          main = title)
+  box()
+  file_name <- paste("Evolution_of_the_grade_for_nature_",i,".png",sep="")
+  dev.print(device = png, file = file_name, width = 600)
+  }
+}
+
+# according to the tranche
+for (i in levels(evolution$TRANCHE_AGE)) {
+  if (length(evolution$global[evolution$TRANCHE_AGE == i] != 0)){
+  title <- paste("Evolution of the grade according to Tranche d'age",i,sep=" ")
+  barplot(t(table(evolution$global[evolution$TRANCHE_AGE == i])), 
+          horiz = FALSE, ylab = "nb of clients", ylim = c(0,70),
+          space = 0.5, las = 1, width = 0.2,
+          main = title)
+  box()
+  file_name <- paste("Evolution_of_the_grade_for_tranche_d_age_",i,".png",sep="")
+  dev.print(device = png, file = file_name, width = 600)
+  }
+}
+
+# according to the marche_csp
+for (i in levels(evolution$MARCHE_CSP)) {
+  if (length(evolution$global[evolution$MARCHE_CSP == i] != 0)){
+    title <- paste("Evolution of the grade according to Marche CSP",i,sep=" ")
+    barplot(t(table(evolution$global[evolution$MARCHE_CSP == i])), 
+            horiz = FALSE, ylab = "nb of clients", ylim = c(0,70),
+            space = 0.5, las = 1, width = 0.2,
+            main = title)
+    box()
+    file_name <- paste("Evolution_of_the_grade_for_marche_CSP_",i,".png",sep="")
+    dev.print(device = png, file = file_name, width = 600)
+  }
+}
+
+# according to the marche_pso
+for (i in levels(evolution$MARCHE_PSO)) {
+  if (length(evolution$global[evolution$MARCHE_PSO == i] != 0)){
+    title <- paste("Evolution of the grade according to Marche PSO",i,sep=" ")
+    barplot(t(table(evolution$global[evolution$MARCHE_PSO == i])), 
+            horiz = FALSE, ylab = "nb of clients", ylim = c(0,70),
+            space = 0.5, las = 1, width = 0.2,
+            main = title)
+    box()
+    file_name <- paste("Evolution_of_the_grade_for_marche_CSP_",i,".png",sep="")
+    dev.print(device = png, file = file_name, width = 600)
+  }
 }
 
